1f980ec7e3a6828684d4a1e082034d0409ddae84
Wellyson Pontes <wellysonpontes@gmail.com>
2022-04-27 21:12:56 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Despesas Pessoais")),
+      body: Center(
+        child: Text("Versão Inicial"),
+      ),
+    );
+  }
+}
