188c1f6ff0bcfad6fba376c4616c2d53ef195bd1
Wellyson Pontes <wellysonpontes@gmail.com>
2022-04-27 22:23:22 -0300

    commit inicial

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,16 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Despesas Pessoais")),
-      body: Center(
-        child: Text("Versão Inicial"),
+      appBar: AppBar(title: Text("Despesas Pessoais 2")),
+      body: Column(
+        children: [
+          Card(
+            child: Text("Gráficos"),
+          ),
+          Card(
+            child: Text("Lista de Transações"),
+          )
+        ],
       ),
     );
   }
