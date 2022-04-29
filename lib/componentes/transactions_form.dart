6944e47200a73dcd316073a05d0c4def41f8d645
Wellyson Pontes <wellysonpontes@gmail.com>
2022-04-28 21:32:51 -0300

    adicionado statefull

--- lib/componentes/transactions_form.dart
+++ lib/componentes/transactions_form.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class TransactionForm extends StatelessWidget {
   final titleControler = TextEditingController();
   final valueControler = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+  TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -30,8 +34,9 @@ class TransactionForm extends StatelessWidget {
                   child: Text("Nova Transação"),
                   textColor: Colors.purple,
                   onPressed: () {
-                    print(titleControler.text);
-                    print(valueControler.text);
+                    final title = titleControler.text;
+                    final value = double.tryParse(valueControler.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                 ),
               ],
