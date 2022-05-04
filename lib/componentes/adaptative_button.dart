cffa8eb95d0c7e502af0a3dfa8e30388bcdd1bc4
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-04 16:10:00 -0300

    Fatoração de adaptative button no transactionForm, de acordo com a plataforma utilizada

---
+++ lib/componentes/adaptative_button.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import '../main.dart';
+
+class AdaptativeButton extends StatelessWidget {
+  final String label;
+  final Function onPressed;
+
+  AdaptativeButton({
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(label),
+            onPressed: () => onPressed(),
+          )
+        : RaisedButton(
+            color: Theme.of(context).colorScheme.primary,
+            child: Text(
+              label,
+              style: Theme.of(context).textTheme.button,
+            ),
+            onPressed: () => onPressed,
+          );
+  }
+}
--- lib/componentes/transactions_form.dart
+++ lib/componentes/transactions_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'adaptative_button.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double, DateTime) onSubmit;
@@ -98,14 +99,19 @@ class _TransactionFormState extends State<TransactionForm> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  RaisedButton(
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Text(
-                      "Nova Transação",
-                      style: Theme.of(context).textTheme.button,
-                    ),
+                  AdaptativeButton(
+                    label: "Nova Transação",
                     onPressed: _submitFomr,
-                  ),
+                  )
+
+                  // RaisedButton(
+                  //   color: Theme.of(context).colorScheme.primary,
+                  //   child: Text(
+                  //     "Nova Transação",
+                  //     style: Theme.of(context).textTheme.button,
+                  //   ),
+                  //   onPressed: _submitFomr,
+                  // ),
                 ],
               ),
             ],
